dc02c615e1afcb904ddee6c2cd5b3983bbd37816
Satyasamsani <119724018+Satyasamsani@users.noreply.github.com>
2023-01-02 20:36:08 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d%d",&i,&n);     if(n>i)     {         printf("Profit");     }     else
        {
            printf("Loss");
        }
        
    }

---
+++ Profit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d%d",&i,&n);
+    if(n>i)
+    {
+        printf("Profit");
+    }
+    else
+    
+    {
+        printf("Loss");
+    }
+    
+}
