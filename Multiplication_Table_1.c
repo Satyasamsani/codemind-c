325019f1f772914e332d73f6c37b5b0031e075f2
Satyasamsani <119724018+Satyasamsani@users.noreply.github.com>
2023-01-26 12:52:59 +0530

    #include<stdio.h> int main() {     int t,a,i;     scanf("%d",&a);     for(i=1;i<=12;i++)     {         printf("%d x %d = %d ",a,i,a*i);     } }
    /*#include<stdio.h>
    int main()
    {
        int n,i;
        scanf("%d",&n);
        for(i=1;i<=12;i++)
        {
            printf("%d x %d = %d
    ",n,i,n*i);
        }
    }*/

---
+++ Multiplication_Table_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int t,a,i;
+    scanf("%d",&a);
+    for(i=1;i<=12;i++)
+    {
+        printf("%d x %d = %d
+",a,i,a*i);
+    }
+}
+
+/*#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=12;i++)
+    {
+        printf("%d x %d = %d
+",n,i,n*i);
+    }
+}*/
