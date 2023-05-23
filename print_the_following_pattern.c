253652e26a9a0d6e7e8d7c2728a3598c9580e0f0
Satyasamsani <119724018+Satyasamsani@users.noreply.github.com>
2023-05-23 19:52:13 +0530

    #include<stdio.h>
    int main()
    {
        int n;
        scanf("%d",&n);
        for(int i=1;i<=n;i++)
        {
            for(int j=1;j<=i;j++)
            {
                if(j==1||j==i||i==n)
                {
                    printf("*");
                }
                else
                {
                    printf(" ");
                }
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
+
 int main()
 {
-    int n,i,j;
+    int n;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(int i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(int j=1;j<=i;j++)
         {
-            printf("%d",j);
+            if(j==1||j==i||i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
